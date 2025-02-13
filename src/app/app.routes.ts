613284d0e57b978a1100b93be318b1252a9a0bec
DrBooleani <notdeyvidsalvatoresemail@gmail.com>
2025-02-13 15:33:43 -0300

    feat: Welcome message

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+  },
+  {
     path: 'builder',
     loadChildren: () => import('./core/builder/builder.routes').then(m => m.buttonRoutes)
   }
