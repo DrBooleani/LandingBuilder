897db3522ae7c03af0bb9c6629d084cdf9de0bbe
DrBooleani <notdeyvidsalvatoresemail@gmail.com>
2025-02-13 14:55:22 -0300

    feat: Sidebar template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { SidebarComponent } from "./core/builder/sidebar/sidebar.component"; // Importação da Sidebar
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, SidebarComponent],
   template: `
-    <router-outlet></router-outlet>
+    <div class="flex">
+      <app-sidebar></app-sidebar>
+      <div class="flex-1">
+        <router-outlet></router-outlet>
+      </div>
+    </div>
   `,
   styles: [],
 })
--- src/app/core/builder/sidebar/sidebar.component.ts
+++ src/app/core/builder/sidebar/sidebar.component.ts
@@ -1,15 +1,24 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
-  template: `
-    <p>
-      sidebar works!
-    </p>
-  `,
-  styles: ``
+  standalone: true,
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css',
+  imports: [NgIf, NgFor]
 })
 export class SidebarComponent {
+  collapsed = false;
+  menuItems = [
+    { label: "Título", icon: "📝" },
+    { label: "Logo", icon: "📷" },
+    { label: "Cores", icon: "🎨" },
+    { label: "Fontes", icon: "🔤" },
+    { label: "Botões", icon: "🔘" },
+  ];
 
+  toggleSidebar() {
+    this.collapsed = !this.collapsed;
+  }
 }
